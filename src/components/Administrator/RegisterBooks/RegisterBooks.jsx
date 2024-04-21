da1ca27811efa0eb539053b722a59e6c64cd2031
Yeison Andrés Marroquín Bernal <121591987+yeisonmarroquin887@users.noreply.github.com>
2024-04-21 15:41:38 -0500

    LiberEdu

--- src/components/Administrator/RegisterBooks/RegisterBooks.jsx
+++ src/components/Administrator/RegisterBooks/RegisterBooks.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
+import './Registerbooks.css'
 
 const RegisterBooks = () => {
   return (
-	<div>
-	  RegisterBooks
-	</div>
+	<section className='Books'>
+	  <h1>Subir libros</h1>
+	  <form className='Books_form' action="">
+		<div>
+			<label htmlFor="">Nombre del libro:</label>
+			<input type="text" required/>
+		</div>
+		<div>
+			<label htmlFor="">Archivo PDF:</label>
+			<input className='Pdf' type="file" />
+		</div>
+		<button>Subir</button>
+	  </form>
+	</section>
   )
 }
 
--- src/components/Administrator/RegisterEstudy/RegisterEstudy.jsx
+++ src/components/Administrator/RegisterEstudy/RegisterEstudy.jsx
@@ -1,10 +1,73 @@
 import React from 'react'
+import './RegisterEstudy.css'
 
 const RegisterEstudy = () => {
   return (
-	<div>
-	  Registrar Estudiante
-	</div>
+	<section className='AddStudentsAndAcudents'>
+	  <article className='Acudents'>
+		<h1>Crear Acudiente</h1>
+		<form className='Acudents_form' action="">
+			<div>
+				<label htmlFor="">Nombres:</label>
+				<input type="text" />
+			</div>
+			<div>
+				<label htmlFor="">Apellidos:</label>
+				<input type="text" />
+			</div>
+			<div>
+				<label htmlFor="">Correo:</label>
+				<input type="email" />
+			</div>
+			<div>
+				<label htmlFor="">Contraseña:</label>
+				<input type="password" />
+			</div>
+			<div>
+				<label htmlFor="">Cedula:</label>
+				<input type="number" />
+			</div>
+			<button>Registrar</button>
+		</form>
+	  </article>
+
+	  <article className='Estudents'>
+	  <h1>Crear Estudiante</h1>
+		<form className='Acudents_form' action="">
+			<div>
+				<label htmlFor="">Nombres:</label>
+				<input type="text" />
+			</div>
+			<div>
+				<label htmlFor="">Apellidos:</label>
+				<input type="text" />
+			</div>
+			<div>
+				<label htmlFor="">Correo:</label>
+				<input type="email" />
+			</div>
+			<div>
+				<label htmlFor="">Contraseña:</label>
+				<input type="password" />
+			</div>
+			<div>
+				<label htmlFor="">No.Identificacion:</label>
+				<input type="number" />
+			</div>
+			<div>
+				<label htmlFor="">CC. Acudiene:</label>
+				<input type="number" />
+			</div>
+			<div>
+				<label htmlFor="">Curso:</label>
+				<select name="" id="">
+					<option value="">Selecciona su curso</option>
+				</select>
+			</div>
+			<button>Registrar</button>
+		</form>
+	  </article>
+	</section>
   )
 }
 

86ca15b6dde4f64dcd957ff48147018e6354205a
Yeison Andrés Marroquín Bernal <121591987+yeisonmarroquin887@users.noreply.github.com>
2024-04-21 11:32:26 -0500

    LiberEdu

--- src/components/Administrator/RegisterDocente/RegisterDoecnte.jsx
+++ src/components/Administrator/RegisterDocente/RegisterDoecnte.jsx
@@ -1,9 +1,46 @@
 import React from 'react'
+import './RegisterDocente.css'
 
 const RegisterDoecnte = () => {
   return (
-	<div>
-	  Registrar docente
+	<div className='AddDocentes'>
+       <h1>Crear Docentes</h1>
+	   <form className='AddDocentes_form' action="">
+		<div>
+        <div>
+			<label htmlFor="">Nombres:</label>
+			<input type="text" required/>
+		</div>
+		<div>
+			<label htmlFor="">Apellidos:</label>
+			<input type="text" required/>
+		</div>
+		<div>
+			<label htmlFor="">Correo:</label>
+			<input type="email" required/>
+		</div>			
+		</div>
+
+		<div className='Select'>
+		<div>
+			<label htmlFor="">Contraseña:</label>
+			<input type="password" required/>
+		</div>
+		<div>
+			<label htmlFor="">Cursos:</label>
+		    <select name="" id="" >
+				<option value="">Selecciona sus Cursos</option>
+			</select>
+		</div>
+		<div>
+			<label htmlFor="">Materias:</label>
+		    <select name="" id="" >
+				<option value="">Selecciona sus materias</option>
+			</select>
+		</div>
+		</div>
+		<button>Registrar</button>
+	   </form>
 	</div>
   )
 }
